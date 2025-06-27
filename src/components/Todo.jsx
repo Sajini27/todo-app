ca87afe154ec4ce42458c159522bbbf0307a6f06
Sajini27 <snrathnaweera27@gmail.com>
2025-06-27 15:44:45 +0530

    Todo component is created
    2025-06-27

---
+++ src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import react from 'react'
+
+const Todo = () => {
+    return(
+        <div className='bg-white place-self-center w-11/12 max-w-md 
+        flex flex-col p-7 min-h-[550px] rounded-xl'>
+{/*--------title------*/}
+        <div className='flexs items-center mt-7 gap-2'>
+            <h1 className='text-3xl font-semibold'>To Do List</h1>
+
+        </div>
+        </div>
+    )
+}
+
+export default Todo
